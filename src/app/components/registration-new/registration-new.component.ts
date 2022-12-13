b17b892cc2d96ae1c9e2d44086e1c0c146169c60
Max-Warden <117695043+Max-Warden@users.noreply.github.com>
2022-12-13 13:30:39 +0200

    Add files via upload

---
+++ src/app/components/registration-new/registration-new.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {RegistrationService} from "../../services/registration.service";
+import {Router} from "@angular/router";
+import {AuthUser} from "../../models/authUser";
+import {AuthService} from "../../services/auth.service";
+
+@Component({
+  selector: 'app-registration-new',
+  templateUrl: './registration-new.component.html',
+  styleUrls: ['./registration-new.component.css']
+})
+export class RegistrationNewComponent implements OnInit {
+
+  constructor(
+    private registrationService:RegistrationService,
+    private router:Router) {
+
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  public onAddNewRegistration(f:NgForm){
+    this.registrationService.addRegistration(f.value).subscribe(()=>{
+      this.router.navigate(["/"]);
+
+    });
+  }
+
+}
---
+++ src/app/models/registration.ts
@@ -0,0 +1,15 @@
+export class Registration{
+  public id:string|null=null;
+  constructor(
+    public name:string,
+    public surname:string,
+    public year:number,
+    public gender:string,
+    public email:string,
+    public phone:string,
+    public Class:string
+   
+  ) {
+  }
+
+}
